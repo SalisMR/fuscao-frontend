82a4caa0723681ab596a9ac036d91c32f98b107c
SalisMR <salisrodrigues365@live.com>
2025-08-10 02:12:33 -0300

    fix(router): usar HashRouter e remover vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import ErrorBoundary from './ErrorBoundary.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
+      <App />
     </HashRouter>
   </React.StrictMode>,
 )
